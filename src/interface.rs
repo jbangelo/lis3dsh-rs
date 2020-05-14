1fa4cbd251557962fbccbc31335986c71cc877c3
Joseph Angelo <joseph.angelo@swift-nav.com>
2020-05-14 00:34:11 -0700

    Added initial implementation

---
+++ src/interface.rs
@@ -0,0 +1,97 @@
+pub trait Interface {
+    type Error;
+
+    fn read_multiple_regs(&mut self, addr: u8, data: &mut [u8]) -> Result<(), Self::Error>;
+
+    fn read_reg(&mut self, addr: u8) -> Result<u8, Self::Error>;
+
+    fn write_u8_reg(&mut self, addr: u8, value: u8) -> Result<(), Self::Error>;
+}
+
+pub use spi::SpiInterface as Spi;
+
+mod spi {
+    use embedded_hal as hal;
+    use hal::{blocking::spi::{Transfer, Write},
+              digital::v2::OutputPin};
+
+    const READ_FLAG: u8 = 0x80;
+    const ADDR_MASK: u8 = 0x7F;
+
+    struct ChipSelectGuard<'a, CSPIN>
+        where CSPIN: OutputPin {
+        cs: &'a mut CSPIN
+    }
+
+    impl<'a, CSPIN> ChipSelectGuard<'a, CSPIN>
+        where CSPIN: OutputPin {
+        fn new(cs: &'a mut CSPIN) -> Self {
+            let _ = cs.set_low();
+            Self {
+                cs
+            }
+        }
+    }
+
+    impl<'a, CSPIN> Drop for ChipSelectGuard<'a, CSPIN>
+        where CSPIN: OutputPin {
+        fn drop(&mut self) {
+            let _ = self.cs.set_high();
+        }
+    }
+
+    pub struct SpiInterface<SPI, CSPIN>
+        where SPI: Transfer<u8> + Write<u8>,
+              CSPIN: OutputPin {
+        spi: SPI,
+        cs: CSPIN,
+    }
+
+    impl<SPI, CSPIN> SpiInterface<SPI, CSPIN>
+        where SPI: Transfer<u8> + Write<u8>,
+              CSPIN: OutputPin {
+        pub fn new(spi: SPI, cs: CSPIN) -> Self {
+            Self {
+                spi,
+                cs,
+            }
+        }
+    }
+
+    impl<SPI, CSPIN> super::Interface for SpiInterface<SPI, CSPIN>
+        where SPI: Transfer<u8> + Write<u8>,
+              CSPIN: OutputPin {
+        type Error = SPI as Transfer<u8>::Error;
+
+        fn read_multiple_regs(&mut self, addr: u8, data: &mut [u8]) -> Result<(), Self::Error> {
+            let _cs = ChipSelectGuard::new(&mut self.cs);
+            let cmd = (addr & ADDR_MASK) | READ_FLAG;
+            let _ = self.spi.transfer(&mut [cmd])?;
+            let _ = self.spi.transfer(data)?;
+            Ok(())
+        }
+
+        fn read_reg(&mut self, addr: u8) -> Result<u8, Self::Error> {
+            let _cs = ChipSelectGuard::new(&mut self.cs);
+            let mut words = [
+                (addr & ADDR_MASK) | READ_FLAG,
+                0
+            ];
+            let reads = self.spi.transfer(&mut words)?;
+            Ok(reads[1])
+        }
+
+        fn write_u8_reg(&mut self, addr: u8, value: u8) -> Result<(), Self::Error> {
+            let _cs = ChipSelectGuard::new(&mut self.cs);
+            let mut words = [
+                addr & ADDR_MASK,
+                value
+            ];
+            let _ = self.spi.transfer(&mut words)?;
+            Ok(())
+        }
+    }
+}
+
+pub mod i2c {}
+
---
+++ src/lib.rs
@@ -0,0 +1,122 @@
+pub mod interface;
+
+extern crate embedded_hal;
+
+use embedded_hal as hal;
+
+
+pub struct Status(u8);
+
+impl Status {
+    const ZYXOR_MASK: u8 = 0x80;
+    const ZOR_MASK: u8 = 0x40;
+    const YOR_MASK: u8 = 0x20;
+    const XOR_MASK: u8 = 0x10;
+    const ZYXDA_MASK: u8 = 0x08;
+    const ZDA_MASK: u8 = 0x04;
+    const YDA_MASK: u8 = 0x02;
+    const XDA_MASK: u8 = 0x01;
+
+    pub fn zyxor(&self) -> bool {
+        (self.0 & Self::ZYXOR_MASK) != 0
+    }
+
+    pub fn zor(&self) -> bool {
+        (self.0 & Self::ZOR_MASK) != 0
+    }
+
+    pub fn yor(&self) -> bool {
+        (self.0 & Self::YOR_MASK) != 0
+    }
+
+    pub fn xor(&self) -> bool {
+        (self.0 & Self::XOR_MASK) != 0
+    }
+
+    pub fn zyxda(&self) -> bool {
+        (self.0 & Self::ZYXDA_MASK) != 0
+    }
+
+    pub fn zda(&self) -> bool {
+        (self.0 & Self::ZDA_MASK) != 0
+    }
+
+    pub fn yda(&self) -> bool {
+        (self.0 & Self::YDA_MASK) != 0
+    }
+
+    pub fn xda(&self) -> bool {
+        (self.0 & Self::XDA_MASK) != 0
+    }
+}
+
+const WHO_AM_I_ADDR: u8 = 0x0f;
+const CTRL_REG4_ADDR: u8 = 0x20;
+const CTRL_REG3_ADDR: u8 = 0x23;
+const STATUS_ADDR: u8 = 0x27;
+const OUT_X_LOW_ADDR: u8 = 0x28;
+const OUT_X_ADDR: u8 = OUT_X_LOW_ADDR;
+
+pub struct Lis3dsh<IFACE>
+    where IFACE: interface::Interface {
+    iface: IFACE,
+}
+
+impl<IFACE> Lis3dsh<IFACE>
+    where IFACE: interface::Interface {
+    fn init<DELAY>(&mut self, delay: &mut DELAY) -> Result<(), IFACE::Error>
+        where DELAY: hal::blocking::delay::DelayMs<u8> {
+        // Reset it
+        self.iface.write_u8_reg(CTRL_REG3_ADDR, 0x01)?;
+        delay.delay_ms(5);
+        // Enable the XYZ channels
+        self.iface.write_u8_reg(CTRL_REG4_ADDR, 0x67)?;
+        self.iface.write_u8_reg(CTRL_REG3_ADDR, 0xE8)
+    }
+
+    pub fn read_data(&mut self) -> Result<[i16; 3], IFACE::Error> {
+        let mut data= [0;6];
+        let _ = self.iface.read_multiple_regs(OUT_X_ADDR, &mut data)?;
+        let x_value = (data[0] as i16) | ((data[1] as i16) << 8);
+        let y_value = (data[2] as i16) | ((data[3] as i16) << 8);
+        let z_value = (data[4] as i16) | ((data[5] as i16) << 8);
+        Ok([x_value, y_value, z_value])
+    }
+
+    pub fn is_data_ready(&mut self) -> Result<bool, IFACE::Error> {
+        Ok(self.status()?.zyxda())
+    }
+
+    pub fn who_am_i(&mut self) -> Result<u8, IFACE::Error> {
+        self.iface.read_reg(WHO_AM_I_ADDR)
+    }
+
+    pub fn status(&mut self) -> Result<Status, IFACE::Error> {
+        let status = self.iface.read_reg(STATUS_ADDR)?;
+        Ok(Status(status))
+    }
+}
+
+impl<SPI, CSPIN> Lis3dsh<interface::Spi<SPI, CSPIN>>
+    where SPI: hal::blocking::spi::Transfer<u8> + hal::blocking::spi::Write<u8>,
+          CSPIN: hal::digital::v2::OutputPin {
+    pub fn new_spi<DELAY>(spi: SPI, mut cs: CSPIN, delay: &mut DELAY) -> Result<Self, interface::Spi<SPI, CSPIN>::Error>
+        where DELAY: hal::blocking::delay::DelayMs<u8> {
+        // Make sure the chip select is in the inactive state
+        let _ = cs.set_high();
+        let iface = interface::Spi::new(spi, cs);
+        let mut accel = Self {
+            iface
+        };
+        accel.init(delay)?;
+        Ok(accel)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
